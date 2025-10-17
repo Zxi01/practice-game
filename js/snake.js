0677a5e59e919138470cc3d5ed0ab2e8a9dc2e69
Zxi01 <cameron.m.jennings@hotmail.com>
2025-10-17 14:40:58 +0100

    Add comments for ease of reading

--- js/snake.js
+++ js/snake.js
@@ -79,18 +79,14 @@ window.onload = function () {
     document.addEventListener("keydown", changeDirection);
 
     // D-pad button event listeners to ensure the buttons are found after DOM loads
-    document
-        .getElementById("dpad-up")
-        ?.addEventListener("click", () => setDirection("Up"));
-    document
-        .getElementById("dpad-down")
-        ?.addEventListener("click", () => setDirection("Down"));
-    document
-        .getElementById("dpad-left")
-        ?.addEventListener("click", () => setDirection("Left"));
-    document
-        .getElementById("dpad-right")
-        ?.addEventListener("click", () => setDirection("Right"));
+    document.getElementById("dpad-up");
+    addEventListener("click", () => setDirection("Up"));
+    document.getElementById("dpad-down");
+    addEventListener("click", () => setDirection("Down"));
+    document.getElementById("dpad-left");
+    addEventListener("click", () => setDirection("Left"));
+    document.getElementById("dpad-right");
+    addEventListener("click", () => setDirection("Right"));
 
     setInterval(update, 1000 / GAME_CONFIG.fps);
     updateScore();
